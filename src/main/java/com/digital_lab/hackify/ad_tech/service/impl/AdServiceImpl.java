af0fa849cddae8e568f1af1d46e3f16dec0c29d0
José Eduardo Cosío <jeduardo.cosio14@gmail.com>
2020-01-18 15:09:23 -0800

    add annotations

--- src/main/java/com/digital_lab/hackify/ad_tech/service/impl/AdServiceImpl.java
+++ src/main/java/com/digital_lab/hackify/ad_tech/service/impl/AdServiceImpl.java
@@ -1,5 +1,8 @@
 package com.digital_lab.hackify.ad_tech.service.impl;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class AdServiceImpl {
 
 }
--- src/main/java/com/digital_lab/hackify/ad_tech/service/impl/UserServiceImpl.java
+++ src/main/java/com/digital_lab/hackify/ad_tech/service/impl/UserServiceImpl.java
@@ -1,16 +1,18 @@
 package com.digital_lab.hackify.ad_tech.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.digital_lab.hackify.ad_tech.domain.model.User;
 import com.digital_lab.hackify.ad_tech.repository.UserRepository;
 import com.digital_lab.hackify.ad_tech.service.UserService;
 
-public class UserServiceImpl implements UserService{
+@Service
+public class UserServiceImpl implements UserService {
 
 	@Autowired
 	UserRepository userRepository;
-	
+
 	@Override
 	public User save(User user) {
 		return userRepository.save(user);
