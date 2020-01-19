f0920d8f9e3ad2095e93276ebfd2d3f663bd8ba6
Efren Cabrera <efren@cabrera.dev>
2020-01-18 18:54:26 -0800

    Add endpoint to create campaigns

--- python_api/app.py
+++ python_api/app.py
@@ -1,6 +1,6 @@
 import configparser
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 from facebook.ad import create_ad, create_creative
 from facebook.ad_set import create_ad_set, get_add_sets
@@ -18,10 +18,22 @@ test_credential = {
     "account_id": config["facebook.sandbox"]["ACCOUNT_ID"]
 }
 
-@app.route("/campaigns")
+@app.route("/campaigns", methods=["GET"])
 def get_campaigns_api():
     campaigns = get_campaigns(**test_credential)
     return jsonify([campaign.get_id() for campaign in campaigns])
+
+@app.route("/campaigns", methods=["POST"])
+def add_campaigns():
+    fields = []
+    params = {
+        "name": request.form["name"],
+        "status": request.form["status"],
+        "special_ad_category": request.form["special_ad_category"],
+        "objective": request.form["objective"]
+    }
+    campaign = create_campaign(**test_credential, fields=fields, params=params)
+    return jsonify({"campaign_id": campaign.get_id()})
 
 @app.route("/")
 def index():
