cea5765f40936ae0e055416f0cd625946fcd7a5e
José Eduardo Cosío <jeduardo.cosio14@gmail.com>
2020-01-18 19:14:24 -0800

    add n

--- src/main/java/com/digital_lab/hackify/ad_tech/web/CampaignController.java
+++ src/main/java/com/digital_lab/hackify/ad_tech/web/CampaignController.java
@@ -2,12 +2,6 @@ package com.digital_lab.hackify.ad_tech.web;
 
 import java.util.List;
 
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.Invocation;
-import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.Response;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -15,7 +9,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -49,18 +42,6 @@ public class CampaignController {
 		return campaignService.save(campaignDto);
 	}
 	
-	@PutMapping
-	public void nothing() {
-		Client client = ClientBuilder.newClient(); 
-		WebTarget webTarget 
-		  = client.target("http://localhost:5000/");
-		WebTarget employeeWebTarget 
-		  = webTarget.path("campaigns");
-		Invocation.Builder invocationBuilderx 
-		  = employeeWebTarget.request();
-//		List<String> response 
-//		  = invocationBuilderx.get(List<String.class>());
-	}
 
 
 }
